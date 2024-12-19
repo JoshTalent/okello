4d51ee45c4dba2d06fe3c5b4256f08c599569a9f
joshTalent <ntwarijosue5@gmail.com>
2024-12-19 02:20:58 -0800

    db changed

--- okello.sql
+++ okello.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2024 at 05:43 PM
+-- Generation Time: Dec 19, 2024 at 11:16 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -33,6 +33,18 @@ CREATE TABLE `gallery` (
   `image` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`id`, `title`, `image`) VALUES
+(8, 'kevin', './images/IGITERANE - Made with PosterMyWall.jpg'),
+(9, 'sam', './images/IGITERANE - Made with PosterMyWall.jpg'),
+(11, 'hellooooooooo', './images/Modern Web Development Services Square (11) - Made with PosterMyWall (1).jpg'),
+(12, 'ldkjoehfhouhfjbcuhwfuohwecnivohv', './images/Business Ads - Made with PosterMyWall.jpg'),
+(13, 'kjcccvwcvuycgvywc', './images/Modern Web Development Services Square (11) - Made with PosterMyWall (1).jpg'),
+(14, 'hey', './images/okello_-removebg-preview.png');
+
 -- --------------------------------------------------------
 
 --
@@ -55,7 +67,8 @@ INSERT INTO `messages` (`id`, `name`, `email`, `subject`, `message`) VALUES
 (1, 'gatare jean marie', 'okellostudio@gmail.com', 'nfjn', 'hello world\r\n'),
 (2, 'ntwari', 'abc@gmail.con', 'report', 'uhfuifhofhillgobvbjbvjbvbveuwevo'),
 (3, 'confy', 'confytradings@gmail', 'hiring', 'vamos'),
-(4, 'tjeice', 'confytradings@gmail', 'hiring', 'qwertyuiopoijhgfxzZxcvn,nbvcfghjbvcxchxcfcccvcvcvcvcbvcbvdftytretetet');
+(4, 'tjeice', 'confytradings@gmail', 'hiring', 'qwertyuiopoijhgfxzZxcvn,nbvcfghjbvcxchxcfcccvcvcvcvcbvcbvdftytretetet'),
+(5, 'mugisha kevin', 'abc@gmail.con', 'greetings', 'hello okello hownare you');
 
 -- --------------------------------------------------------
 
@@ -64,10 +77,19 @@ INSERT INTO `messages` (`id`, `name`, `email`, `subject`, `message`) VALUES
 --
 
 CREATE TABLE `nitify` (
+  `image` varchar(250) NOT NULL,
   `id` int(11) NOT NULL,
   `title` varchar(200) NOT NULL,
   `notification` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `nitify`
+--
+
+INSERT INTO `nitify` (`image`, `id`, `title`, `notification`) VALUES
+('./images/Business Ads - Made with PosterMyWall.jpg', 11, ';kvlejnvjbvjkebvb ', 'n djbvhbvbvuievwuu'),
+('./images/Modern Web Development Services Square (11) - Made with PosterMyWall (1).jpg', 12, 'xnjnsjkdcidvuiv jhjc', 'ndklcnlcnhqeifufwubdjcn cevnhwi');
 
 -- --------------------------------------------------------
 
@@ -95,7 +117,7 @@ CREATE TABLE `okello` (
 --
 
 INSERT INTO `okello` (`id`, `name`, `about`, `birth`, `website`, `phone`, `city`, `age`, `degree`, `email`, `freelance`, `password`) VALUES
-(1, 'OKELLO Studios', 'A professional photographer is a skilled artist who captures moments and creates images using a camera. They possess a deep understanding of lighting, composition, and technical aspects of photography. With their expertise, they transform ordinary scenes into extraordinary visuals, telling stories and evoking emotions through their work.\r\n\r\nA professional videographer is a skilled artist who uses cameras and editing software to capture and create visually engaging videos. They possess a keen eye for composition and lighting, and understand how to tell a story through moving images. Videographers often work on a variety of projects, such as weddings, corporate events, music videos, and documentaries. They collaborate with clients to understand their vision and then use their technical expertise to bring that vision to life.', '18 / 02 / 2003', '    www.okellostudio.rw', 72787636, ' Kigali , RWANDA', 786026717, ' masters', 'ntwarijosue5@gmail.com', 'booked', '1234');
+(1, 'OKELLO Studios', 'A professional photographer is a skilled artist who captures moments and creates images using a camera. They possess a deep understanding of lighting, composition, and technical aspects of photography. With their expertise, they transform ordinary scenes into extraordinary visuals, telling stories and evoking emotions through their work.\r\n\r\nA professional videographer is a skilled artist who uses cameras and editing software to capture and create visually engaging videos. They possess a keen eye for composition and lighting, and understand how to tell a story through moving images. Videographers often work on a variety of projects, such as weddings, corporate events, music videos, and documentaries. They collaborate with clients to understand their vision and then use their technical expertise to bring that vision to life.', '18 / 02 / 2003', '    www.okellostudio.rw', 786026717, ' Kigali , RWANDA', 786026717, ' masters', 'ntwarijosue5@gmail.com', 'booked', '1234');
 
 --
 -- Indexes for dumped tables
@@ -133,19 +155,19 @@ ALTER TABLE `okello`
 -- AUTO_INCREMENT for table `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `nitify`
 --
 ALTER TABLE `nitify`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `okello`
